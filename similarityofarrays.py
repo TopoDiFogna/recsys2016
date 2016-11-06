5b8f89ccc98076efce052f1297401c714c494d36
Paolo Paterna <paolo.paterna@gmail.com>
2016-11-06 19:08:33 +0100

    filtering items per user

--- similarityofarrays.py
+++ similarityofarrays.py
@@ -1,17 +1,34 @@
 import pandas as pd
 import numpy as np
+from datetime import datetime as dt
 
 
-def square(x): return x**2
+def square(x): return x ** 2
 
-items= pd.read_table("data/item_profile.csv",header=0,sep="\t")
+items = pd.read_table("data/item_profile.csv", header=0, sep="\t")
+sample = pd.read_table("data/sample_submission.csv", sep=",", header=0)
+interactions = pd.read_table("data/interactions.csv", sep="\t", header=0)
+
+# TODO prendere in inglesso gli utenti sample
+colFuckingName = sample.columns[0]
+sample = sample.rename(columns={colFuckingName: 'user_id'})
+sampleIds = sample.user_id.values  # array contenente i sample user_id
 
 
-##TODO prendere in inglesso gli utenti sample
-##TODO cercare i lavori con iterazione prendendo quelli con rating massimo
-## TODO droppare le colonne di tags e title dal dataFrame
+# TODO cercare i lavori con iterazione prendendo quelli con rating massimo
+def getuserratings(userid):
+    sampleinteractions = interactions.loc[interactions['user_id'] == userid]
+    sampleinteractions = sampleinteractions.groupby(by='item_id', group_keys=False).apply(lambda x: x.ix[x.interaction_type.idxmax()])
+    return sampleinteractions
 
-for index, first_row in items.iterrows():#solo quelli cliccati
+tic = dt.now()
+print(getuserratings(2835411))
+print(dt.now()-tic)
+
+# TODO droppare le colonne di tags e title dal dataFrame
+
+
+for index, first_row in items.iterrows():  # solo quelli cliccati
     for index, second_row in items.iterrows():
 
         s_tag = 0
@@ -20,7 +37,7 @@ for index, first_row in items.iterrows():#solo quelli cliccati
         j_tag_len = 0
         i_title_len = 0
         j_title_len = 0
-        s_i_j=0
+        s_i_j = 0
 
         if not (isinstance(first_row["tags"], float)):
             a = np.array(first_row["tags"].split(","))
@@ -45,23 +62,23 @@ for index, first_row in items.iterrows():#solo quelli cliccati
                 pass
         else:
             pass
-    i_series=first_row.drop(["title","tags"])
-    j_series=second_row.drop(["title","tags"])
+    i_series = first_row.drop(["title", "tags"])
+    j_series = second_row.drop(["title", "tags"])
 
-    result_series=i_series.multiply(j_series)
-    result_series["similarity"]= result_series.sum(axis=1)
-    similarity=result_series["similarity"].values.item(0)
+    result_series = i_series.multiply(j_series)
+    result_series["similarity"] = result_series.sum(axis=1)
+    similarity = result_series["similarity"].values.item(0)
 
-    i_series=i_series.apply(square)
-    j_series=j_series.apply(square)
-    i_series["sum"]=i_series.sum(axis=1)
+    i_series = i_series.apply(square)
+    j_series = j_series.apply(square)
+    i_series["sum"] = i_series.sum(axis=1)
     j_series["sum"] = j_series.sum(axis=1)
     i_sum = i_series["sum"].value.item(0)
     j_sum = j_series["sum"].value.item(0)
-    denominator=(i_sum+i_tag_len+i_title_len)**0.5 * (j_sum+j_title_len+j_tag_len)**0.5
+    denominator = (i_sum + i_tag_len + i_title_len) ** 0.5 * (j_sum + j_title_len + j_tag_len) ** 0.5
 
-    similarity=similarity/denominator + s_title/denominator + s_tag/denominator
+    similarity = similarity / denominator + s_title / denominator + s_tag / denominator
 
-##TODO calcolare i rating
+# TODO calcolare i rating
 
-##TODO sputare i 5 più alti
+# TODO sputare i 5 più alti
