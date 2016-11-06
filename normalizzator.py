03c3a256937ff325f93b2789e6b069bd29abb356
Paolo Paterna <paolo.paterna@gmail.com>
2016-11-06 20:54:51 +0100

    Fixed the normalizator!
    result.fillna(0, inplace=True)

--- normalizzator.py
+++ normalizzator.py
@@ -1,38 +1,38 @@
 import pandas as pd
-from  datetime import datetime as dt
+from datetime import datetime as dt
 import numpy as np
 
-items = pd.read_table("data\item_profile.csv",header=0,sep="\t")
-tic=dt.now()
-result= pd.DataFrame(index=range(len(items.index.values)), columns=items.columns.values)
+items = pd.read_table("data\item_profile.csv", header=0, sep="\t")
+tic = dt.now()
+result = pd.DataFrame(index=range(len(items.index.values)), columns=items.columns.values)
 
-for index,row in items.iterrows() :
+for index, row in items.iterrows():
 
     row["career_level"] /= 6
     row["discipline_id"] /= 23
-    row["industry_id"] /=23
+    row["industry_id"] /= 23
 
-    country=row["country"]
-    if country == "de" :
+    country = row["country"]
+    if country == "de":
         row["country"] = 0.33
-    elif country == "at" :
+    elif country == "at":
         row["country"] = 0.66
-    elif country == "ch" :
+    elif country == "ch":
         row["country"] = 0.99
     elif country == "non_dach":
         row["country"] = 0
 
     row["region"] /= 16
 
-    latitude=row["latitude"]
+    latitude = row["latitude"]
     if isinstance(latitude, np.float64):
         latitude = 0
-    elif latitude <0 :
+    elif latitude < 0:
         latitude = 90 + abs(latitude)
-    row["latitude"] = latitude/180
+    row["latitude"] = latitude / 180
 
     longitude = row["longitude"]
-    if isinstance(longitude,np.float64):
+    if isinstance(longitude, np.float64):
         longitude = 0
     elif longitude < 0:
         longitude = 180 + abs(longitude)
@@ -42,9 +42,9 @@ for index,row in items.iterrows() :
 
     row["employment"] /= 5
 
-    result.loc[index]=row
+    result.loc[index] = row
 
-result.fillna(0)
+result.fillna(0, inplace=True)
 
 with open("normalized.csv", "w") as f:
-    result.to_csv(f, sep=',',index=False)
+    result.to_csv(f, sep=',', index=False)
