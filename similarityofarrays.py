610904493997c863ed059a31a7d17cdaedadfede
Paolo Paterna <paolo.paterna@gmail.com>
2016-11-06 22:55:35 +0100

    corrected sep in reading items normalized csv

--- similarityofarrays.py
+++ similarityofarrays.py
@@ -5,7 +5,7 @@ from datetime import datetime as dt
 
 def square(x): return x ** 2
 
-items = pd.read_table("data/item_profile.csv", header=0, sep="\t")
+items = pd.read_table("normalized.csv", header=0, sep=",")
 sample = pd.read_table("data/sample_submission.csv", sep=",", header=0)
 interactions = pd.read_table("data/interactions.csv", sep="\t", header=0)
 
@@ -29,56 +29,56 @@ print(dt.now()-tic)
 # TODO droppare le colonne di tags e title dal dataFrame
 
 
-# for index, first_row in items.iterrows():  # solo quelli cliccati
-#     for index, second_row in items.iterrows():
-#
-#         s_tag = 0
-#         s_title = 0
-#         i_tag_len = 0
-#         j_tag_len = 0
-#         i_title_len = 0
-#         j_title_len = 0
-#         s_i_j = 0
-#
-#         if not (isinstance(first_row["tags"], float)):
-#             a = np.array(first_row["tags"].split(","))
-#             j_tag_len = len(a)
-#             if not (first_row != second_row and isinstance(second_row["tags"], float)):
-#                 b = np.array(second_row["tags"].split(","))
-#                 i_tag_len = len(b)
-#                 s_tag = len(np.intersect1d(a, b))
-#             else:
-#                 pass
-#         else:
-#             pass
-#
-#         if not (isinstance(first_row["title"], float)):
-#             c = np.array(first_row["title"].split(","))
-#             j_title_len = len(c)
-#             if not (first_row != second_row and isinstance(second_row["title"], float)):
-#                 d = np.array(second_row["title"].split(","))
-#                 i_title_len = len(d)
-#                 s_title = len(np.intersect1d(c, d))
-#             else:
-#                 pass
-#         else:
-#             pass
-#     i_series = first_row.drop(["title", "tags"])
-#     j_series = second_row.drop(["title", "tags"])
-#
-#     result_series = i_series.multiply(j_series)
-#     result_series["similarity"] = result_series.sum(axis=1)
-#     similarity = result_series["similarity"].values.item(0)
-#
-#     i_series = i_series.apply(square)
-#     j_series = j_series.apply(square)
-#     i_series["sum"] = i_series.sum(axis=1)
-#     j_series["sum"] = j_series.sum(axis=1)
-#     i_sum = i_series["sum"].value.item(0)
-#     j_sum = j_series["sum"].value.item(0)
-#     denominator = (i_sum + i_tag_len + i_title_len) ** 0.5 * (j_sum + j_title_len + j_tag_len) ** 0.5
-#
-#     similarity = similarity / denominator + s_title / denominator + s_tag / denominator
+for index, first_row in items.iterrows():  # solo quelli cliccati
+    for index, second_row in items.iterrows():
+
+        s_tag = 0
+        s_title = 0
+        i_tag_len = 0
+        j_tag_len = 0
+        i_title_len = 0
+        j_title_len = 0
+        s_i_j = 0
+
+        if not (isinstance(first_row["tags"], float)):
+            a = np.array(first_row["tags"].split(","))
+            j_tag_len = len(a)
+            if not (first_row != second_row and isinstance(second_row["tags"], float)):
+                b = np.array(second_row["tags"].split(","))
+                i_tag_len = len(b)
+                s_tag = len(np.intersect1d(a, b))
+            else:
+                pass
+        else:
+            pass
+
+        if not (isinstance(first_row["title"], float)):
+            c = np.array(first_row["title"].split(","))
+            j_title_len = len(c)
+            if not (first_row != second_row and isinstance(second_row["title"], float)):
+                d = np.array(second_row["title"].split(","))
+                i_title_len = len(d)
+                s_title = len(np.intersect1d(c, d))
+            else:
+                pass
+        else:
+            pass
+    i_series = first_row.drop(["title", "tags"])
+    j_series = second_row.drop(["title", "tags"])
+
+    result_series = i_series.multiply(j_series)
+    result_series["similarity"] = result_series.sum(axis=1)
+    similarity = result_series["similarity"].values.item(0)
+
+    i_series = i_series.apply(square)
+    j_series = j_series.apply(square)
+    i_series["sum"] = i_series.sum(axis=1)
+    j_series["sum"] = j_series.sum(axis=1)
+    i_sum = i_series["sum"].value.item(0)
+    j_sum = j_series["sum"].value.item(0)
+    denominator = (i_sum + i_tag_len + i_title_len) ** 0.5 * (j_sum + j_title_len + j_tag_len) ** 0.5
+
+    similarity = similarity / denominator + s_title / denominator + s_tag / denominator
 
 # TODO calcolare i rating
 
