d460152c9156d9300e83db820fbfb43164a01c42
Paolo Paterna <paolo.paterna@gmail.com>
2017-01-28 10:52:24 +0100

    Inner product submission

--- utils/cfutils.py
+++ utils/cfutils.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime as dt
 import numpy as np
 from scipy.sparse import coo_matrix
-from utils.dataloading import *
+from utils.dataloading import load_sparse_csc
 from scipy.sparse import vstack
 import math as m
 
@@ -14,12 +14,14 @@ import math as m
 # jobrolesdf = pd.read_csv("../precomputedData/jobrole_matrix.csv", header=0)
 interactions = pd.read_table("data/interactions.csv", sep="\t", header=0)
 
-matrix_similarity = load_sparse_csc("../precomputedData/userRatingSimilarity_IP.npz").tocsr()
+matrix_similarity = load_sparse_csc("precomputedData/userRatingSimilarity_IP.npz").tocsr()
 rating_user_array = interactions.user_id.unique().tolist()
+
 
 def save_sparse_csc(filename, array):
     np.savez(filename, data=array.data, indices=array.indices,
              indptr=array.indptr, shape=array.shape)
+
 
 def create_user_rating_matrix():
     items_series = pd.Series(index=items.id, data=np.arange(items.index.size))
@@ -35,7 +37,7 @@ def create_user_rating_matrix():
         for item in item_clicked:
             rows.append(index)
             columns.append(items_series.loc[int(item)])
-            data.append(interaction_filtered[interaction_filtered["item_id"] == item].values.size)
+            data.append(interaction_filtered[interaction_filtered["item_id"] == item].interaction_type.values.size)
         index += 1
         print("User computed in {}".format(dt.now() - tic))
     user_rating_matrix = coo_matrix((data, (rows, columns)), shape=(len(rating_user_array), items_series.size))
@@ -44,36 +46,39 @@ def create_user_rating_matrix():
     user_rating_matrix = user_rating_matrix.tocsc()
     save_sparse_csc("../precomputedData/user_rating_matrix_IP", user_rating_matrix)
 
+
 def normalize_user_rating_matrix():
     user_rating_matrix = load_sparse_csc("../precomputedData/user_rating_matrix.npz").tocsr()
     lenght_vector = np.squeeze(np.asarray(user_rating_matrix.sum(axis=1)))
     index = 0
     user_rating_matrix_Normalized = user_rating_matrix.getrow(index) / m.sqrt(lenght_vector.item(index))
-    lenght_vector = np.delete(lenght_vector,index)
-    for item in lenght_vector :
+    lenght_vector = np.delete(lenght_vector, index)
+    for item in lenght_vector:
         index = index + 1
         matrix_row = user_rating_matrix.getrow(index)
-        if(item == 0) :
+        if (item == 0):
             user_rating_matrix_Normalized = vstack([user_rating_matrix_Normalized, matrix_row])
         else:
-            user_rating_matrix_Normalized = vstack([user_rating_matrix_Normalized, matrix_row/m.sqrt(item)])
+            user_rating_matrix_Normalized = vstack([user_rating_matrix_Normalized, matrix_row / m.sqrt(item)])
 
-    save_sparse_csc("../precomputedData/userRatingsMatrixNormalized.npz",user_rating_matrix_Normalized.tocsc())
+    save_sparse_csc("../precomputedData/userRatingsMatrixNormalized.npz", user_rating_matrix_Normalized.tocsc())
+
 
 def create_user_rating_matrix_similarity():
     user_rating_matrix = load_sparse_csc("../precomputedData/user_rating_matrix_IP.npz")
     num_rows = user_rating_matrix.shape[0]
     rows_array = np.arange(num_rows)
 
-    matrix_similarity = np.dot(user_rating_matrix.getrow(0),user_rating_matrix.T)
-    for index in rows_array :
+    matrix_similarity = np.dot(user_rating_matrix.getrow(0), user_rating_matrix.T)
+    for index in rows_array:
         tic = dt.now()
-        if (index != 0) :
-            new_row = np.dot(user_rating_matrix.getrow(index),user_rating_matrix.T)
+        if (index != 0):
+            new_row = np.dot(user_rating_matrix.getrow(index), user_rating_matrix.T)
             matrix_similarity = vstack([matrix_similarity, new_row])
-        print("user {} computed in: {}".format(index,dt.now() - tic))
+        print("user {} computed in: {}".format(index, dt.now() - tic))
 
-    save_sparse_csc("../precomputedData/userRatingSimilarity_IP.npz",matrix_similarity.tocsc())
+    save_sparse_csc("../precomputedData/userRatingSimilarity_IP.npz", matrix_similarity.tocsc())
+
 
 def get_top_n_similar_users(user_id, n):
     top_indexes = []
